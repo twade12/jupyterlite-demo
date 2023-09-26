399bd82ef572b2a468cb885f003b07684e45354f
Resensys <57818865+twade12@users.noreply.github.com>
2023-09-26 17:51:42 -0500

    Update db_helper.py

--- content/senstream/db_helper.py
+++ content/senstream/db_helper.py
@@ -1,3 +1,4 @@
+import mysql.connector
 import numpy as np
 import pandas as pd
 
@@ -5,6 +6,16 @@ class Helper:
   def __init__(self,name):
     self.name = name
     print("The helper has been created")
+
+  def connect(self):
+    cnx = mysql.connector.connect(user="tom",password="Manhattan44joke#",host="resensys.net",port=3306)
+    query = "SELECT * FROM admin.Accounts;"
+    c = cnx.cursor()
+    c.execute(query)
+    results = c.fetchall()
+
+    cnx.close()
+    print("results: ",results)
 
   def test1(self,message):
     print(f"The helper printed message: {message}")
